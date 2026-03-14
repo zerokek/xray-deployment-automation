ffa7bb2ac76842247a3541594d12360e76f865da
zerokek <zerokek23@gmail.com>
2026-03-14 16:35:32 +0300

    Update init_server.sh

--- init_server.sh
+++ init_server.sh
@@ -20,19 +20,25 @@ add_repository() {
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
 }
+deployment() {
+    log "EXEC" "Собираем образ"
+    docker build -t xray-zeroke:1.0.0beta .
+    log "EXEC" "Создаем контейнер и пробрасываем порты"
+    docker run  -p 2083:443 -d xray-zeroke:1.0.0beta
+    log "INFO" "Сборка закончена"
+}
 install_dependencies() {
  
 
- log "EXEC" "Обновление пакетов"
- apt update
- log "EXEC" "Установка зависимостей"
- apt install ca-certificates curl -y
- add_keys
- add_repository
- apt update
- log "EXEC" "Установка Docker"
- apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
- log "EXEC" "Скачивание образа"
-
+    log "EXEC" "Обновление пакетов"
+    apt update
+    log "EXEC" "Установка зависимостей"
+    apt install ca-certificates curl  -y
+    add_keys
+    add_repository
+    apt update
+    log "EXEC" "Установка Docker"
+    apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+    deployment
 }
 install_dependencies
